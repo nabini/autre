888291728aaea51d491c2c15e236521126c034e4
Kone <kone@gmail.com>
2022-03-23 16:34:34 +0100

    m

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+ <h1>Images 1</h1>
+ <img src="/images/m10.jpg"  width={200} alt="" />
+ <h2>Images 1</h2>
+ <img src="/images/m14.jpg"  width={200} alt="" />
+
+ <h3>Images 1</h3>
+ <img src="/images/m15.jpg"  width={200} alt="" />
+
+ <h4>Images 1</h4>
+ <img src="/images/m12.jpg"  width={200} alt="" />
+
+
+    </div>
+  );
+}
+
+export default App;
